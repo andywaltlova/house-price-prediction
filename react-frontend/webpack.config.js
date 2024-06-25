e18c6f883863254ccf726ebc596d22ee3189138b
Andrea Waltlova <andywaltlova@gmail.com>
2024-06-26 00:39:13 +0200

    Remove unused env var
    Signed-off-by: Andrea Waltlova <andywaltlova@gmail.com>

--- react-frontend/webpack.config.js
+++ react-frontend/webpack.config.js
@@ -1,13 +1,9 @@
 const path = require('path');
-const webpack = require("webpack");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'src/index.html'),
-    }),
-    new webpack.DefinePlugin({
-        "process.env.REACT_APP_BACKEND_URL": JSON.stringify(process.env.REACT_APP_BACKEND_URL)
     }),
   ],
   module: {
