71b07294ba1afcd4c4342793c6d3d3f3932f6d9f
Andrea Waltlova <andywaltlova@gmail.com>
2024-06-27 17:43:05 +0200

    Adjust predict endpoint to save prediction to DB and return current table content
    Signed-off-by: Andrea Waltlova <andywaltlova@gmail.com>

--- flask-backend/app.py
+++ flask-backend/app.py
@@ -6,10 +6,35 @@ import pandas as pd
 import os
 
 from model_utils.main import load_model, predict
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import func
 from validators import PredictSchema
 
 app = Flask(__name__, static_folder='/app/static')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+
 model = load_model('./model_utils/model.joblib')
+
+db = SQLAlchemy(app)
+
+class PredictedPrice(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    longitude = db.Column(db.Float, nullable=False)
+    latitude = db.Column(db.Float, nullable=False)
+    housing_median_age = db.Column(db.Float, nullable=False)
+    total_rooms = db.Column(db.Float, nullable=False)
+    total_bedrooms = db.Column(db.Float, nullable=False)
+    population = db.Column(db.Float, nullable=False)
+    households = db.Column(db.Float, nullable=False)
+    median_income = db.Column(db.Float, nullable=False)
+    is_less_than_1h_ocean = db.Column(db.Boolean, nullable=False, default=False)
+    is_inland = db.Column(db.Boolean, nullable=False, default=False)
+    is_island = db.Column(db.Boolean, nullable=False, default=False)
+    is_near_bay = db.Column(db.Boolean, nullable=False, default=False)
+    is_near_ocean = db.Column(db.Boolean, nullable=False, default=False)
+    price= db.Column(db.Float, nullable=False)
+    created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
+
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
@@ -18,6 +43,7 @@ def serve(path):
         return send_from_directory(app.static_folder, path)
     else:
         return send_from_directory(app.static_folder, 'index.html')
+
 
 @app.route('/api/predict', methods=['POST', 'OPTIONS'])
 def predict_price():
@@ -33,31 +59,43 @@ def predict_price():
         response = jsonify({"error": "No data provided"})
         return _corsify_actual_response(response), 400
 
+    db_name_map = {
+        'ocean_proximity_<1H OCEAN': 'is_less_than_1h_ocean',
+        'ocean_proximity_INLAND': 'is_inland',
+        'ocean_proximity_ISLAND': 'is_island',
+        'ocean_proximity_NEAR BAY': 'is_near_bay',
+        'ocean_proximity_NEAR OCEAN': 'is_near_ocean'
+    }
+
     try:
         # Attempt to deserialize and validate data
+        for key, value in db_name_map.items():
+            data[value] = bool(data.pop(key, 0))
+
+        if ocean_proximity := data.pop('ocean_proximity', None):
+            key = db_name_map.get(f'ocean_proximity_{ocean_proximity}', None)
+            if key:
+                data[key] = True
+
         schema = PredictSchema()
         validated_data = schema.load(data)
     except ValidationError as err:
         response = jsonify(err.messages)
         return _corsify_actual_response(response), 400
 
-    ocean_proximity = validated_data.pop('ocean_proximity')
-
-    validated_data['ocean_proximity_<1H OCEAN'] = 0
-    validated_data['ocean_proximity_INLAND'] = 0
-    validated_data['ocean_proximity_ISLAND'] = 0
-    validated_data['ocean_proximity_NEAR BAY'] = 0
-    validated_data['ocean_proximity_NEAR OCEAN'] = 0
-
-    validated_data[f"ocean_proximity_{ocean_proximity}"] = 1
-
     df = pd.DataFrame([validated_data])
+    predicted_price = predict(df, model)
 
-    price = predict(df, model)
-    # Return the series as a JSON object
-    response = jsonify({"price": round(price[0], 8)})
+    validated_data['price'] = round(predicted_price[0],8)
 
-    return _corsify_actual_response(response)
+    prediction_row = PredictedPrice(**validated_data)
+    db.session.add(prediction_row)
+    db.session.commit()
+
+    predicts_schema = PredictSchema(many=True)
+    all_predicts = PredictedPrice.query.order_by(PredictedPrice.created_at.desc()).all()
+    result = predicts_schema.dump(all_predicts)
+    return _corsify_actual_response(jsonify(result))
 
 def _build_cors_preflight_response():
     response = make_response()
@@ -73,4 +111,6 @@ def _corsify_actual_response(response):
     return response
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(host='0.0.0.0', port=5000)
--- flask-backend/validators.py
+++ flask-backend/validators.py
@@ -3,15 +3,16 @@ from marshmallow import Schema, fields, validate
 class PredictSchema(Schema):
     longitude = fields.Float(required=True)
     latitude = fields.Float(required=True)
-    housing_median_age = fields.Float(required=True)
-    total_rooms = fields.Float(required=True)
-    total_bedrooms = fields.Float(required=True)
-    population = fields.Float(required=True)
-    households = fields.Float(required=True)
+    housing_median_age = fields.Float(required=True, validate=validate.Range(min=0))
+    total_rooms = fields.Float(required=True, validate=validate.Range(min=0))
+    total_bedrooms = fields.Float(required=True, validate=validate.Range(min=0))
+    population = fields.Float(required=True, validate=validate.Range(min=0))
+    households = fields.Float(required=True, validate=validate.Range(min=0))
     median_income = fields.Float(required=True)
-    ocean_proximity = fields.Str(
-        required=True,
-        validate=validate.OneOf([
-            '<1H OCEAN', 'INLAND', 'ISLAND', 'NEAR BAY', 'NEAR OCEAN'
-        ])
-    )
+    is_less_than_1h_ocean = fields.Boolean(required=True)
+    is_inland = fields.Boolean(required=True)
+    is_island = fields.Boolean(required=True)
+    is_near_bay = fields.Boolean(required=True)
+    is_near_ocean = fields.Boolean(required=True)
+    price = fields.Float(nullable=True, validate=validate.Range(min=0))
+    created_at = fields.DateTime(dump_only=True)
